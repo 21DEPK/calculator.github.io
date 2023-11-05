dd68b077959224a0649320678ddad49779e1a6ab
DEEPAK <110716963+21DEPK@users.noreply.github.com>
2023-11-05 09:54:49 +0530

    index.js

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 let currentValue = '';
-
+let key;
 function numericButtonClick(button) {
     let numericElement = document.querySelector(`.numeric-${button}`);
     numericElement.addEventListener("click", ()=>{
@@ -7,45 +7,74 @@ function numericButtonClick(button) {
         document.querySelector('input').value = `${currentValue}`
     })
 }
+function updateValue(currentValue){
+    document.querySelector('input').value = `${currentValue}`;
+}
+
+window.addEventListener('keypress',(e)=>{
+    key = e.key;
+    switch(key){
+        case '0': currentValue += '0'; updateValue(currentValue);break;
+        case '1': currentValue += '1'; updateValue(currentValue);break;
+        case '2': currentValue += '2'; updateValue(currentValue);break;
+        case '3': currentValue += '3'; updateValue(currentValue);break;
+        case '4': currentValue += '4'; updateValue(currentValue);break;
+        case '5': currentValue += '5'; updateValue(currentValue);break;
+        case '6': currentValue += '6'; updateValue(currentValue);break;
+        case '7': currentValue += '7'; updateValue(currentValue);break;
+        case '8': currentValue += '8'; updateValue(currentValue);break;
+        case '9': currentValue += '9'; updateValue(currentValue);break;
+        case '+': currentValue += '+'; updateValue(currentValue);break;
+        case '-': currentValue += '-'; updateValue(currentValue);break;
+        case '/': currentValue += '/'; updateValue(currentValue);break;
+        case '.': currentValue += '.'; updateValue(currentValue);break;
+        case '*': currentValue += '*'; updateValue(currentValue);break;
+        case '(': currentValue += '('; updateValue(currentValue);break;
+        case ')': currentValue += ')'; updateValue(currentValue);break;
+        case 'Enter': currentValue = eval(currentValue);updateValue(currentValue);break;
+        default: currentValue += '';
+    }
+});
+
 let divide = document.querySelector(".operation-divide");
 divide.addEventListener("click", ()=>{
     currentValue += '/' ;
-    document.querySelector('input').value = `${currentValue}`;
+    updateValue(currentValue);
 })
 let multiply = document.querySelector(".operation-multiply");
 multiply.addEventListener("click", ()=>{
     currentValue += '*' ;
-    document.querySelector('input').value = `${currentValue}`;
+    updateValue(currentValue);
 })
 let addition = document.querySelector(".operation-addition");
 addition.addEventListener("click", ()=>{
     currentValue += '+' ;
-    document.querySelector('input').value = `${currentValue}`;
+    updateValue(currentValue);
 })
 let subtraction = document.querySelector(".operation-subtraction");
 subtraction.addEventListener("click", ()=>{
     currentValue += '-' ;
-    document.querySelector('input').value = `${currentValue}`;
+    updateValue(currentValue);
 })
 let clear = document.querySelector(".clear-last-one");
 clear.addEventListener("click",()=>{
     currentValue=currentValue.slice(0,currentValue.length-1);
-    document.querySelector('input').value = `${currentValue}`;
+    updateValue(currentValue);
 })
 let clearAll = document.querySelector(".operation-clear");
 clearAll.addEventListener("click", ()=>{
     currentValue='';
-    document.querySelector('input').value = `${currentValue}`;
+    updateValue(currentValue);
 })
 let dot = document.querySelector(".dot");
 dot.addEventListener("click", ()=>{
     currentValue+='.';
-    document.querySelector('input').value = `${currentValue}`;
+    updateValue(currentValue);
 })
 let equal = document.querySelector(".equal");
 equal.addEventListener("click",()=>{
     currentValue = eval(currentValue);
-    document.querySelector('input').value = `${currentValue}`;
+    updateValue(currentValue);
 })
 numericButtonClick(0);
 numericButtonClick(1);
